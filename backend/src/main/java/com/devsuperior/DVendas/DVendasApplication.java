c0da0e719f7f0ce0d828ebba7bb74dfb8c32a21b
Gabriel Tomac <gabrielftomac@gmail.com>
2021-11-04 20:26:37 -0300

    Projeto Criado - Dev Superior

---
+++ backend/src/main/java/com/devsuperior/DVendas/DVendasApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.DVendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DVendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/DVendas/DVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.DVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
